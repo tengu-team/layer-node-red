5a5fda513c759daa7193dfb685bcef4ce22197b9
sebastien pattyn <sebastien.pattyn@gmail.com>
2017-11-21 10:32:58 +0100

    first commit

---
+++ reactive/layer_node_red.py
@@ -0,0 +1,17 @@
+from charms.reactive import when, when_not, set_state
+
+
+@when_not('layer-node-red.installed')
+def install_layer_node_red():
+    # Do your setup here.
+    #
+    # If your charm has other dependencies before it can install,
+    # add those as @when() clauses above., or as additional @when()
+    # decorated handlers below
+    #
+    # See the following for information about reactive charms:
+    #
+    #  * https://jujucharms.com/docs/devel/developer-getting-started
+    #  * https://github.com/juju-solutions/layer-basic#overview
+    #
+    set_state('layer-node-red.installed')
